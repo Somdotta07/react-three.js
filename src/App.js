d0829ae3ba11c08ccef7675f86ea0061210db9f7
Somdotta07 <jhilick.s@gmail.com>
2022-05-27 16:07:02 +0200

    Add Physics, usePlane, useBox with canon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Stars } from '@react-three/drei';
+import { Physics,usePlane, useBox } from '@react-three/cannon'
 import './App.css';
 function Box() {
+  const [ref, api] = useBox(() =>({mass: 1, position: [0,10,0]}))
   return (
-    <mesh>
+    <mesh onClick={() => {
+      api.velocity.set(0, 2, 0);
+    }} ref={ref} position={[0,2,0]}>
       <boxBufferGeometry attach="geometry" />
       <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  )
+}
+
+function Plane() {
+    const [ref] = usePlane(() => ({
+
+    rotation: [-Math.PI / 2, 0, 0],
+
+    args:[100,100]
+
+  }));
+  return (
+    <mesh ref={ref} rotation= {[-Math.PI /2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]}/>
+      <meshLambertMaterial attach="material" color="lightblue" />
     </mesh>
   )
 }
@@ -14,8 +34,14 @@ function App() {
   return (
     <Canvas>
       <OrbitControls />
+      <Stars />
       <ambientLight intensity={0.5} />
-      <Box />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <Physics>
+        <Box />
+        <Plane />
+      </Physics>
+    
     </Canvas>
   );
 }
